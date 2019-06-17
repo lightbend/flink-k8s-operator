a8712800a828a8c9363f1fd9c40d3954cfc2ff55
blublinsky <boris.lublinsky@lightbend.com>
2019-06-16 20:10:01 -0500

    Refactored deployer

--- operator/src/main/scala/com/lightbend/operator/KubernetesFlinkClusterDeployer.scala
+++ operator/src/main/scala/com/lightbend/operator/KubernetesFlinkClusterDeployer.scala
@@ -60,17 +60,7 @@ class KubernetesFlinkClusterDeployer(client: KubernetesClient, entityName: Strin
       portBuild(8081, "ui"))
 
     // Environment variables
-    var envVars = new ListBuffer[EnvVar]()
-    envVars += envBuild("CONTAINER_METRIC_PORT", params.metric_query_port)
-    envVars += envBuild("JOBMANAGER_MEMORY", s"${params.master_memory}m")
-    envVars += envBuild("JOB_MANAGER_RPC_ADDRESS", s"$name-$OPERATOR_TYPE_MASTER_LABEL")
-    params.mounts.foreach (mount => envVars += envBuild(mount.getEnvname, mount.getMountdirectory))
-    params.parallelism match {
-      case p if(p != 1)  => envVars += envBuild(Constants.PARALLELISM_ENV_VAR, params.parallelism.toString)
-      case _ =>
-    }
-    if (cluster.getEnv != null)
-      cluster.getEnv.asScala.foreach(env => envVars += envBuild(env.getName, env.getValue))
+    val envVars = buildEnv(cluster, params, true)
 
     // Arguments
     val args = params.master_args.toList
@@ -89,38 +79,10 @@ class KubernetesFlinkClusterDeployer(client: KubernetesClient, entityName: Strin
       ("memory" -> new QuantityBuilder().withAmount(s"${params.master_memory}Mi").build()))
 
     // Container
-    val containerBuilder = new ContainerBuilder()
-      .withImage(params.imageRef)
-      .withImagePullPolicy(params.pullPolicy)
-      .withName(OPERATOR_TYPE_MASTER_LABEL)
-      .withTerminationMessagePolicy("File")
-      .withEnv(envVars.asJava)
-      .withPorts(ports.asJava)
-      .withLivenessProbe(masterLiveness)
-      .withArgs(args.asJava)
-      .withResources(new ResourceRequirementsBuilder().withLimits(limits.asJava).build())
+    val containerBuilder = cBuilder(params, envVars, ports , args, limits).withLivenessProbe(masterLiveness)
 
     // Mounts
-    val volumes = new ListBuffer[Volume]
-    params.mounts foreach{mount =>
-      val readonly = mount.getResourcetype match {
-        case v if v.equalsIgnoreCase("PVC") =>
-          volumes += new VolumeBuilder().withName(mount.getEnvname.toLowerCase).withPersistentVolumeClaim(
-            new PersistentVolumeClaimVolumeSource(mount.getResourcename, false)).build()
-          false
-        case v if v.equalsIgnoreCase("CONFIGMAP") =>
-          volumes += new VolumeBuilder().withName(mount.getEnvname.toLowerCase.toLowerCase).withConfigMap(
-            new ConfigMapVolumeSourceBuilder().withName(mount.getResourcename).build()).build()
-          true
-        case _ =>
-          volumes += new VolumeBuilder().withName(mount.getEnvname.toLowerCase).withSecret(
-            new SecretVolumeSourceBuilder().withSecretName(mount.getResourcename).build()).build()
-          true
-      }
-      containerBuilder.addToVolumeMounts(new VolumeMountBuilder()
-        .withName(mount.getEnvname.toLowerCase).withMountPath(mount.getMountdirectory).withReadOnly(readonly)
-        .build())
-    }
+    val volumes = volumesBuilder(containerBuilder, params)
 
     // Metrics
     var annotations = Map[String, String]()
@@ -132,21 +94,7 @@ class KubernetesFlinkClusterDeployer(client: KubernetesClient, entityName: Strin
     var labels = getLabels(cluster, OPERATOR_TYPE_MASTER_LABEL)
 
     // Replication controller
-    new ReplicationControllerBuilder()
-      .withNewMetadata
-        .withName(podName)
-        .withNamespace(namespace)
-      .endMetadata
-      .withNewSpec
-        .withReplicas(1)
-        .withNewTemplate
-          .withNewMetadata
-            .withAnnotations(annotations.asJava)
-            .withLabels(labels.asJava)
-          .endMetadata
-          .withNewSpec.withContainers(containerBuilder.build).withVolumes(volumes.toList.asJava).endSpec()
-        .endTemplate
-      .endSpec.build
+    controllerBuilder(podName, namespace, 1, annotations, labels, containerBuilder, volumes)
   }
 
   private def getRCforWorker(cluster: FlinkCluster, params: FlinkParams, namespace: String): ReplicationController = {
@@ -163,21 +111,7 @@ class KubernetesFlinkClusterDeployer(client: KubernetesClient, entityName: Strin
       portBuild(params.metric_query_port.toInt, "metric"))
 
     // Environment variables
-    var envVars = new ListBuffer[EnvVar]()
-    envVars += envBuild("CONTAINER_METRIC_PORT", params.metric_query_port)
-    envVars += envBuild("TASKMANAGER_MEMORY", s"${params.worker_memory}m")
-    envVars += envBuild("TASKMANAGER_SLOTS", params.worker_slots.toString)
-    envVars += envBuild("JOB_MANAGER_RPC_ADDRESS", s"$name-$OPERATOR_TYPE_MASTER_LABEL")
-    envVars += new EnvVarBuilder().withName("K8S_POD_IP").withValueFrom(
-      new EnvVarSourceBuilder().withFieldRef(
-        new ObjectFieldSelectorBuilder().withFieldPath("status.podIP").build()).build()).build
-    params.mounts.foreach (mount => envVars += envBuild(mount.getEnvname, mount.getMountdirectory))
-    params.parallelism match {
-      case p if(p != 1)  => envVars += envBuild(Constants.PARALLELISM_ENV_VAR, params.parallelism.toString)
-      case _ =>
-    }
-    if (cluster.getEnv != null)
-      cluster.getEnv.asScala.foreach(env => envVars += envBuild(env.getName, env.getValue))
+    val envVars = buildEnv(cluster, params, false)
 
     // Arguments
     val args = List(OPERATOR_TYPE_WORKER_LABEL)
@@ -188,37 +122,10 @@ class KubernetesFlinkClusterDeployer(client: KubernetesClient, entityName: Strin
 
 
     // Container
-    val containerBuilder = new ContainerBuilder()
-      .withImage(params.imageRef)
-      .withImagePullPolicy(params.pullPolicy)
-      .withName(OPERATOR_TYPE_WORKER_LABEL)
-      .withTerminationMessagePolicy("File")
-      .withEnv(envVars.asJava)
-      .withPorts(ports.asJava)
-      .withArgs(args.asJava)
-      .withResources(new ResourceRequirementsBuilder().withLimits(limits.asJava).build())
+    val containerBuilder = cBuilder(params, envVars, ports , args, limits)
 
     // Mounts
-    val volumes = new ListBuffer[Volume]
-    params.mounts foreach{mount =>
-      val readonly = mount.getResourcetype match {
-        case v if v.equalsIgnoreCase("PVC") =>
-          volumes += new VolumeBuilder().withName(mount.getEnvname.toLowerCase).withPersistentVolumeClaim(
-            new PersistentVolumeClaimVolumeSource(mount.getResourcename, false)).build()
-          false
-        case v if v.equalsIgnoreCase("CONFIGMAP") =>
-          volumes += new VolumeBuilder().withName(mount.getEnvname.toLowerCase).withConfigMap(
-            new ConfigMapVolumeSourceBuilder().withName(mount.getResourcename).build()).build()
-          true
-        case _ =>
-          volumes += new VolumeBuilder().withName(mount.getEnvname.toLowerCase).withSecret(
-            new SecretVolumeSourceBuilder().withSecretName(mount.getResourcename).build()).build()
-          true
-      }
-      containerBuilder.addToVolumeMounts(new VolumeMountBuilder()
-        .withName(mount.getEnvname.toLowerCase).withMountPath(mount.getMountdirectory).withReadOnly(readonly)
-        .build())
-    }
+    val volumes = volumesBuilder(containerBuilder, params)
 
     // Metrics
     var annotations = Map[String, String]()
@@ -227,24 +134,10 @@ class KubernetesFlinkClusterDeployer(client: KubernetesClient, entityName: Strin
     }
 
     // Labels
-    var labels = getLabels(cluster, OPERATOR_TYPE_WORKER_LABEL)
+    val labels = getLabels(cluster, OPERATOR_TYPE_WORKER_LABEL)
 
     // Replication controller
-    new ReplicationControllerBuilder()
-      .withNewMetadata
-        .withName(podName)
-        .withNamespace(namespace)
-      .endMetadata
-      .withNewSpec
-        .withReplicas(params.worker_instances)
-        .withNewTemplate
-          .withNewMetadata
-            .withAnnotations(annotations.asJava)
-            .withLabels(labels.asJava)
-          .endMetadata
-          .withNewSpec.withContainers(containerBuilder.build).withVolumes(volumes.toList.asJava).endSpec
-        .endTemplate
-      .endSpec.build
+    controllerBuilder(podName, namespace, params.worker_instances, annotations, labels, containerBuilder, volumes)
   }
 
   private def envBuild(key: String, value: String): EnvVar = new EnvVarBuilder().withName(key).withValue(value).build
@@ -262,6 +155,89 @@ class KubernetesFlinkClusterDeployer(client: KubernetesClient, entityName: Strin
     if(component != null)
       labels = labels + ("component" -> component)
     labels
+  }
+
+  private def buildEnv(cluster: FlinkCluster, params: FlinkParams, jobmanager : Boolean) : Seq[EnvVar] = {
+    var envVars = new ListBuffer[EnvVar]()
+    envVars += envBuild("CONTAINER_METRIC_PORT", params.metric_query_port)
+    jobmanager match {
+      case true =>
+        envVars += envBuild("JOBMANAGER_MEMORY", s"${params.master_memory}m")
+        envVars += envBuild("JOB_MANAGER_RPC_ADDRESS", s"${cluster.getName}-$OPERATOR_TYPE_MASTER_LABEL")
+      case _ =>
+        envVars += envBuild("TASKMANAGER_MEMORY", s"${params.worker_memory}m")
+        envVars += envBuild("TASKMANAGER_SLOTS", params.worker_slots.toString)
+        envVars += envBuild("JOB_MANAGER_RPC_ADDRESS", s"${cluster.getName}-$OPERATOR_TYPE_MASTER_LABEL")
+        envVars += new EnvVarBuilder().withName("K8S_POD_IP").withValueFrom(
+          new EnvVarSourceBuilder().withFieldRef(
+            new ObjectFieldSelectorBuilder().withFieldPath("status.podIP").build()).build()).build
+
+    }
+    params.mounts.foreach (mount => envVars += envBuild(mount.getEnvname, mount.getMountdirectory))
+    params.parallelism match {
+      case p if(p != 1)  => envVars += envBuild(Constants.PARALLELISM_ENV_VAR, params.parallelism.toString)
+      case _ =>
+    }
+    if (cluster.getEnv != null)
+      cluster.getEnv.asScala.foreach(env => envVars += envBuild(env.getName, env.getValue))
+
+    envVars
+  }
+
+  private def cBuilder(params: FlinkParams, envVars : Seq[EnvVar], ports : List[ContainerPort], args: List[String], limits: Map[String, Quantity]) : ContainerBuilder = {
+    new ContainerBuilder()
+      .withImage(params.imageRef)
+      .withImagePullPolicy(params.pullPolicy)
+      .withName(OPERATOR_TYPE_MASTER_LABEL)
+      .withTerminationMessagePolicy("File")
+      .withEnv(envVars.asJava)
+      .withPorts(ports.asJava)
+      .withArgs(args.asJava)
+      .withResources(new ResourceRequirementsBuilder().withLimits(limits.asJava).build())
+  }
+
+  private def volumesBuilder(containerBuilder: ContainerBuilder, params: FlinkParams) : Seq[Volume] = {
+    // Mounts
+    val volumes = new ListBuffer[Volume]
+    params.mounts foreach {mount =>
+      val readonly = mount.getResourcetype match {
+        case v if v.equalsIgnoreCase ("PVC") =>
+          volumes += new VolumeBuilder ().withName (mount.getEnvname.toLowerCase).withPersistentVolumeClaim (
+          new PersistentVolumeClaimVolumeSource (mount.getResourcename, false) ).build ()
+          false
+        case v if v.equalsIgnoreCase ("CONFIGMAP") =>
+          volumes += new VolumeBuilder ().withName (mount.getEnvname.toLowerCase).withConfigMap (
+          new ConfigMapVolumeSourceBuilder ().withName (mount.getResourcename).build () ).build ()
+          true
+        case _ =>
+          volumes += new VolumeBuilder ().withName (mount.getEnvname.toLowerCase).withSecret (
+          new SecretVolumeSourceBuilder ().withSecretName (mount.getResourcename).build () ).build ()
+          true
+      }
+      containerBuilder.addToVolumeMounts (new VolumeMountBuilder ()
+        .withName (mount.getEnvname.toLowerCase).withMountPath (mount.getMountdirectory).withReadOnly (readonly)
+        .build () )
+    }
+    volumes
+  }
+
+  private def controllerBuilder(podname: String, namespace : String, replicas : Int, annotations : Map[String, String], labels : Map[String, String], containerBuilder: ContainerBuilder, volumes : Seq[Volume]) : ReplicationController = {
+
+    new ReplicationControllerBuilder()
+      .withNewMetadata
+        .withName(podname)
+        .withNamespace(namespace)
+      .endMetadata
+      .withNewSpec
+        .withReplicas(replicas)
+        .withNewTemplate
+          .withNewMetadata
+            .withAnnotations(annotations.asJava)
+            .withLabels(labels.asJava)
+          .endMetadata
+          .withNewSpec.withContainers(containerBuilder.build).withVolumes(volumes.toList.asJava).endSpec()
+        .endTemplate
+      .endSpec.build
   }
 
   def getDefaultLabels(name: String): Map[String, String] = {
